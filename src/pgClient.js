f4ff60da0c608a333978bc20e29bf8c49c87ecab
listenaddress <tbobrien612@gmail.com>
2020-02-03 13:42:10 -0800

    Fix race condition

--- src/pgClient.js
+++ src/pgClient.js
@@ -1,11 +1,19 @@
 const { Client } = require('pg')
+const connectionString = process.env.LOTUS_DB
+let client
 
 require('dotenv').config()
 
-const connectionString = process.env.LOTUS_DB
-
-const client = new Client({ connectionString })
+const startPGClient = async () => {
+  try {
+    client = await new Client(connectionString)
+    console.log('Connected to database')
+  } catch (error) {
+    throw error
+  }
+}
 
 module.exports = {
+  startPGClient,
   client
 }
